f31e645ba1a171eb07934d5f2c27a6b4a385227c
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-05-21 12:09:48 -0300

    feat: adding

--- two-sum.js
+++ two-sum.js
@@ -1,3 +1,12 @@
 const array = [1, 4, 7, 2];
 
-const getPair = (arr) => {};
+const getPair = (arr, target) => {
+  current = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+  }
+
+  console.log("current", current);
+};
+
+getPair(array);
